7ea2c380cd37a9860cfbc7695b66ff3a4ab46d7d
Felix Arntz <felix-arntz@t-online.de>
2016-01-26 19:22:59 +0100

    improved maintenance template handler

--- web/app/mu-plugins/WPPRSC/Base/Maintenance.php
+++ web/app/mu-plugins/WPPRSC/Base/Maintenance.php
@@ -45,33 +45,8 @@ class Maintenance extends \WPPRSC\BaseAbstract {
 		$this->send_headers();
 
 		if ( $template ) {
-			require_once $template;
-			return;
+			require $template;
 		}
-
-		?>
-<!DOCTYPE html>
-<html <?php echo $data['language_attributes']; ?>>
-	<head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		<meta http-equiv="x-ua-compatible" content="ie=edge">
-		<title><?php echo $data['title']; ?></title>
-		<link rel="stylesheet" href="https://cdn.rawgit.com/twbs/bootstrap/v4-dev/dist/css/bootstrap.min.css">
-		<script src="https://cdn.rawgit.com/twbs/bootstrap/v4-dev/dist/js/bootstrap.min.js"></script>
-	</head>
-	<body>
-		<div class="container">
-			<div class="row">
-				<main id="main" class="col-md-12">
-					<h1><?php echo $data['title']; ?></h1>
-					<p><?php echo $data['description']; ?></p>
-				</main>
-			</div>
-		</div>
-	</body>
-</html>
-		<?php
 	}
 
 	protected function setup_data( $template ) {
@@ -124,6 +99,8 @@ class Maintenance extends \WPPRSC\BaseAbstract {
 		if ( defined( 'WP_DEFAULT_MAINTENANCE_TEMPLATE' ) && WP_DEFAULT_MAINTENANCE_TEMPLATE && 'maintenance.php' !== ltrim( WP_DEFAULT_MAINTENANCE_TEMPLATE, '/' ) ) {
 			$locations[] = WP_CONTENT_DIR . '/' . ltrim( WP_DEFAULT_MAINTENANCE_TEMPLATE, '/' );
 		}
+
+		$locations[] = wpprsc_get_path( 'templates/maintenance.php' );
 
 		foreach ( $locations as $location ) {
 			if ( file_exists( $location ) ) {
