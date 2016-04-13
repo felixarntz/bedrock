5b1778f65a750a1c56b41e83a80186396cfdbc26
felixarntz <felix-arntz@t-online.de>
2016-04-13 08:47:19 +0200

    improved Git manager to be able to pull, push, reset to commit

--- web/app/mu-plugins/WPPRSC/Modules/GitManager.php
+++ web/app/mu-plugins/WPPRSC/Modules/GitManager.php
@@ -10,6 +10,9 @@ class GitManager extends \WPPRSC\ModuleAbstract {
 	private $root_dir;
 	private $branch_name;
 
+	private $admin_message = '';
+	private $admin_error = false;
+
 	protected function __construct( $args = array() ) {
 		parent::__construct( $args );
 
@@ -19,10 +22,13 @@ class GitManager extends \WPPRSC\ModuleAbstract {
 
 	public function run() {
 		add_action( 'admin_bar_init', array( $this, 'init' ) );
+
+		add_action( 'admin_action_git_push', array( $this, 'action_git_push' ) );
+		add_action( 'admin_action_git_pull', array( $this, 'action_git_pull' ) );
+		add_action( 'admin_action_git_reset_commit', array( $this, 'action_git_reset_commit' ) );
 	}
 
 	public function init() {
-		$cap = $this->get_setting( 'capability' );
 		if ( ! current_user_can( $this->get_setting( 'capability' ) ) ) {
 			return;
 		}
@@ -56,6 +62,81 @@ class GitManager extends \WPPRSC\ModuleAbstract {
 		add_action( 'admin_head', array( $this, 'print_admin_bar_styles' ) );
 	}
 
+	public function action_git_push() {
+		wp_reset_vars( array( 'action' ) );
+
+		if ( ! current_user_can( $this->get_setting( 'capability' ) ) ) {
+			return;
+		}
+
+		$output = $this->exec( 'push', array( '--all' ) );
+		if ( ! isset( $output[0] ) ) {
+			$this->hook_admin_notice( __( 'An unknown error occurred while trying to push latest changes to remote.', 'wpprsc' ), true );
+			return;
+		}
+
+		if ( 0 === strpos( $output[0], 'Permission denied' ) ) {
+			$this->hook_admin_notice( __( 'Permission denied. Could not push latest changes to remote.', 'wpprsc' ), true );
+			return;
+		}
+
+		if ( 0 === strpos( $output[0], 'Everything up-to-date' ) ) {
+			$this->hook_admin_notice( __( 'The remote is already up to date with the local changes. Nothing was pushed.', 'wpprsc' ) );
+			return;
+		}
+
+		$this->hook_admin_notice( __( 'Changes pushed to remote successfully.', 'wpprsc' ) );
+	}
+
+	public function action_git_pull() {
+		wp_reset_vars( array( 'action' ) );
+
+		if ( ! current_user_can( $this->get_setting( 'capability' ) ) ) {
+			return;
+		}
+
+		$output = $this->exec( 'pull', array(), array( 'SSH_AGENT_PID' => '1704' ) );
+		if ( ! isset( $output[0] ) ) {
+			$this->hook_admin_notice( __( 'An unknown error occurred while trying to pull latest changes from remote.', 'wpprsc' ), true );
+			return;
+		}
+
+		if ( 0 === strpos( $output[0], 'Permission denied' ) ) {
+			$this->hook_admin_notice( __( 'Permission denied. Could not pull latest changes from remote.', 'wpprsc' ), true );
+			return;
+		}
+
+		if ( 0 === strpos( $output[0], 'Already up-to-date' ) ) {
+			$this->hook_admin_notice( __( 'The local changes are already up to date with the remote. Nothing was pulled.', 'wpprsc' ) );
+			return;
+		}
+
+		$this->hook_admin_notice( __( 'Changes pulled from remote successfully.', 'wpprsc' ) );
+	}
+
+	public function action_git_reset_commit() {
+		$commit_hash = $_REQUEST['commit'];
+
+		wp_reset_vars( array( 'action', 'commit' ) );
+
+		if ( ! current_user_can( $this->get_setting( 'capability' ) ) ) {
+			return;
+		}
+
+		$output = $this->exec( 'reset', array( '--hard', $commit_hash ) );
+		if ( ! isset( $output[0] ) ) {
+			$this->hook_admin_notice( sprintf( __( 'An unknown error occurred while trying to reset to commit %s.', 'wpprsc' ), esc_html( $commit_hash ) ), true );
+			return;
+		}
+
+		if ( 0 === strpos( $output[0], 'fatal:' ) ) {
+			$this->hook_admin_notice( sprintf( __( '%s is not a valid commit hash to reset to.', 'wpprsc' ), esc_html( $commit_hash ) ), true );
+			return;
+		}
+
+		$this->hook_admin_notice( sprintf( __( 'Local repository reset to commit %s.', 'wpprsc' ), esc_html( $commit_hash ) ) );
+	}
+
 	public function show_admin_bar_info( $admin_bar ) {
 		$commits = $this->get_commits();
 		$unpushed_commits = $this->get_commits( array( 'mode' => 'unpushed' ), OBJECT_K );
@@ -78,10 +159,39 @@ class GitManager extends \WPPRSC\ModuleAbstract {
 
 		$admin_bar->add_node( $main_node_args );
 
+		$admin_bar->add_group( array(
+			'id'		=> 'git-actions',
+			'parent'	=> 'git-info',
+		) );
+
+		if ( 0 < count( $unpushed_commits ) ) {
+			$admin_bar->add_node( array(
+				'id'		=> 'git-action-push',
+				'parent'	=> 'git-actions',
+				'title'		=> __( 'Push changes to remote', 'wpprsc' ),
+				'href'		=> $this->add_admin_query_arg( 'action', 'git_push' ),
+			) );
+		}
+
+		$admin_bar->add_node( array(
+			'id'		=> 'git-action-pull',
+			'parent'	=> 'git-actions',
+			'title'		=> __( 'Pull changes from remote', 'wpprsc' ),
+			'href'		=> $this->add_admin_query_arg( 'action', 'git_pull' ),
+		) );
+
+		$admin_bar->add_group( array(
+			'id'		=> 'git-commits',
+			'parent'	=> 'git-info',
+			'meta'		=> array(
+				'class'		=> 'ab-sub-secondary',
+			),
+		) );
+
 		foreach ( $commits as $commit ) {
 			$args = array(
-				'id'		=> 'git-info-commit-' . $commit['commit_hash_abbrev'],
-				'parent'	=> 'git-info',
+				'id'		=> 'git-commit-' . $commit['commit_hash_abbrev'],
+				'parent'	=> 'git-commits',
 				'title'		=> $commit['commit_message'] . ' [' . $commit['commit_hash_abbrev'] . ']',
 			);
 			if ( isset( $unpushed_commits[ $commit['commit_hash'] ] ) ) {
@@ -91,6 +201,15 @@ class GitManager extends \WPPRSC\ModuleAbstract {
 			}
 
 			$admin_bar->add_node( $args );
+			$admin_bar->add_node( array(
+				'id'		=> 'git-commit-' . $commit['commit_hash_abbrev'] . '-reset',
+				'parent'	=> 'git-commit-' . $commit['commit_hash_abbrev'],
+				'title'		=> __( 'Reset to this commit', 'wpprsc' ),
+				'href'		=> $this->add_admin_query_arg( array(
+					'action'	=> 'git_reset_commit',
+					'commit'	=> $commit['commit_hash'],
+				) ),
+			) );
 		}
 	}
 
@@ -223,6 +342,25 @@ class GitManager extends \WPPRSC\ModuleAbstract {
 		return $output;
 	}
 
+	public function hook_admin_notice( $message, $error = false ) {
+		$this->admin_message = $message;
+		$this->admin_error = $error;
+
+		add_action( 'admin_notices', array( $this, 'show_notice' ) );
+	}
+
+	public function show_notice() {
+		$class = $this->admin_error ? 'notice-error' : 'notice-success';
+		?>
+		<div class="notice <?php echo $class; ?> is-dismissible">
+			<p>
+				<strong><?php _e( 'Git:', 'wpprsc' ); ?></strong>
+				<?php echo $this->admin_message; ?>
+			</p>
+		</div>
+		<?php
+	}
+
 	private function escape_shell_arg( $arg ) {
 		$os = strtoupper( substr( php_uname( 's' ), 0, 3 ) );
 
@@ -281,6 +419,18 @@ class GitManager extends \WPPRSC\ModuleAbstract {
 		}
 
 		return '';
+	}
+
+	private function add_admin_query_arg( $key, $value = '' ) {
+		$url = false;
+		if ( ! is_admin() ) {
+			$url = admin_url( '/' );
+		}
+
+		if ( is_array( $key ) ) {
+			return add_query_arg( $key, $url );
+		}
+		return add_query_arg( $key, $value, $url );
 	}
 
 	protected function get_default_args() {
